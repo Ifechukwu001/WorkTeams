fbf2dd014d2e23f538055de6e7e0ce38b6da8777
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-01-03 11:01:36 +0100

    Setups

---
+++ setup_database.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Script to setup the database.
+
+sudo apt update -y
+sudo apt install -y mysql-server
+sudo service mysql start
+
+sudo mysql << _EOF_
+CREATE DATABASE IF NOT EXISTS wt_db;
+CREATE USER IF NOT EXISTS 'wt_user'@'localhost' IDENTIFIED BY 'wt_pass';
+GRANT ALL PRIVILEGES ON wt_db.* TO 'wt_user'@'localhost';
+FLUSH PRIVILEGES;
+_EOF_
+
+if [ -z "$WT_USER" ]
+	sed -i "\$a\export WT_STORAGE=db WT_USER=wt_user WT_HOST=localhost WT_PASS=wt_pass WT_DB=wt_db" /etc/profile
+	sudo reboot
+fi
