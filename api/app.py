d809a68ef9aba6e7cf06e73becb54a676c32618f
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-02-15 03:17:58 +0100

    Add CORS

--- api/app.py
+++ api/app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """The API App module"""
 from flask import Flask
+from flask_cors import CORS
 from api.endpoints import api_views
 
 app = Flask(__name__)
 app.register_blueprint(api_views, url_prefix="/api")
-
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5001")
