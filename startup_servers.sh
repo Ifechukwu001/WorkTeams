e3390608a4447e9922b48288dd2209036ab89b73
Ifechukwu T. Ogidi <ogidiifechukwu5@gmail.com>
2023-02-24 12:52:47 +0100

    Startup servers modified

--- startup_servers.sh
+++ startup_servers.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 # Start up the web server and api.
 
-cd ~/Workteams && gunicorn --bind 0.0.0.0:80 wsgi:web &
-cd ~/Workteams && gunicorn --bind 0.0.0.0:5001 wsgi:api &
+sudo apt install authbind
+sudo touch /etc/authbind/byport/80
+sudo chmod 500 /etc/authbind/byport/80
+sudo chown $USER /etc/authbind/byport/80
+cd ~/WorkTeams && authbind gunicorn --bind 0.0.0.0:80 wsgi:web &
+cd ~/WorkTeams && gunicorn --bind 0.0.0.0:5001 wsgi:api &
