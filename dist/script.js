9056b76cbc1a91fa44cc35708cf3c94e31abb50b
Daniel Udechukwu <danieludechukwu117@gmail.com>
2023-04-04 12:21:46 +0100

    Finished login page - TailwindCSS

---
+++ dist/script.js
@@ -0,0 +1,7 @@
+// Get Current Year
+
+const currentYear = document.getElementById("currentYear")
+let date = new Date()
+let year = date.getFullYear()
+
+currentYear.innerHTML = year
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    "./dist/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+    // colors: {
+    //   transparent: 'transparent',
+    //   current: 'currentColor',
+    //   black: colors.black,
+    //   white: colors.white,
+    //   gray: colors.gray,
+    //   emerald: colors.emerald,
+    //   indigo: colors.indigo,
+    //   yellow: colors.yellow,
+    //   orange: colors.orange,
+    //   blue: colors.blue,
+      
+    //   whitesmoke: "#f9f5eb",
+    //   tan: "#e4dccf",
+    //   navy: "#002b5b",
+    //   red: "#ea5455",
+    //   teal: "#66bfbf",
+    //   aqua: "#eaf6f6",
+    //   pink: "#ff0063"
+    // }
+  },
+  plugins: [],
+}
+
