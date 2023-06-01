83a3c1ca55612d8b86ab5bba01a09fbcd9bfb0bb
Daniel Udechukwu <danieludechukwu117@gmail.com>
2023-06-01 11:43:56 +0100

    Test Commit

--- dist/script.js
+++ dist/script.js
@@ -5,3 +5,26 @@ let date = new Date()
 let year = date.getFullYear()
 
 currentYear.innerHTML = year
+
+document.getElementById('signup').addEventListener('click', () =>{
+  const userEmail = document.getElementById('email').value
+  const userPassword = document.getElementById('password').value
+
+  
+  fetch('https://ifechukwu.pythonanywhere.com/api/user', {
+    method: 'POST',
+    body: JSON.stringify({
+      email: userEmail,
+      password: userPassword
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+  fetch('https://ifechukwu.pythonanywhere.com/api/user')
+    .then(response => response.json())
+    .then(data => console.log(data)) 
+
+})
+
